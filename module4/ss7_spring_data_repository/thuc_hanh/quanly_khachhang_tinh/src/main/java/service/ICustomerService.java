ebcd8a351bcf3672265f479efa375ae7ece0b454
unknown <ngominhhung3991@gmail.com>
2021-11-03 22:06:17 +0700

    update

---
+++ module4/ss7_spring_data_repository/thuc_hanh/quanly_khachhang_tinh/src/main/java/service/ICustomerService.java
@@ -0,0 +1,8 @@
+package service;
+
+import model.Customer;
+import model.Province;
+
+public interface ICustomerService extends IGeneralService<Customer>{
+    Iterable<Customer> findAllByProvince(Province province);
+}
